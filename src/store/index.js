35bfa12fa141fdfe828872baf8a7d274d292b006
Beksii <heliumvalley@gmail.com>
2024-04-14 03:51:05 +0600

    redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userSlice from '../features/user/userSlice'
 import todosSlice from '../features/todos/todosSlice'
 import postsSlice from '../features/posts/postsSlice'
 import notifySlice from '../features/notify/notifySlice'
+import storage from 'redux-persist/lib/storage'
+import { persistStore,
+     persistReducer,
+     FLUSH,
+     REGISTER,
+     PAUSE,
+     PERSIST,
+     PURGE,
+     REHYDRATE
+    
+    } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export const store = configureStore({
-    reducer: {
+const rootReducer = combineReducers({
         user: userSlice,
         todos: todosSlice,
         posts: postsSlice,
         notify: notifySlice
-    },
 })
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoreActions: [FLUSH,REGISTER,PAUSE,PERSIST,PURGE,REHYDRATE],
+        },
+    }) 
+})
+
+
+export const persistor = persistStore(store); 
+export default store;
